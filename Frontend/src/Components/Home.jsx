f465af8c26dfd5b7b2f1811530d2a092a57433dd
ajay <ajay@sgate.net>
2023-11-30 18:42:37 +0530

    submit

--- Frontend/src/Components/Home.jsx
+++ Frontend/src/Components/Home.jsx
@@ -55,7 +55,7 @@ const Home = () => {
   return (
     <div>
       <div className='p-3 d-flex justify-content-around mt-3'>
-        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25 rounded'>
           <div className='text-center pb-1'>
             <h4>Admin</h4>
           </div>
@@ -65,7 +65,7 @@ const Home = () => {
             <h5>{adminTotal}</h5>
           </div>
         </div>
-        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25 rounded'>
           <div className='text-center pb-1'>
             <h4>Employee</h4>
           </div>
@@ -75,7 +75,7 @@ const Home = () => {
             <h5>{employeeTotal}</h5>
           </div>
         </div>
-        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25 rounded'>
           <div className='text-center pb-1'>
             <h4>Salary</h4>
           </div>
